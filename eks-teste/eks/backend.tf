56c59db3531b42f223f680a0d14a78becc945fe6
fabio romao <fromao@mkmservice.com>
2022-01-27 20:58:27 -0300

    fela

--- eks-teste/eks/backend.tf
+++ eks-teste/eks/backend.tf
@@ -2,8 +2,8 @@ terraform {
     backend "s3" {
     bucket = "romaoeks-eks-eks"
     encrypt = false
-    key = "idiotamaster"
+    key = "idiota"
     region = "sa-east-1"
-    profile = "terraeks" # you have to give the profile name here. not the variable("${var.AWS_PROFILE}")
+    #profile = "terraeks" # you have to give the profile name here. not the variable("${var.AWS_PROFILE}")
   }
 }
