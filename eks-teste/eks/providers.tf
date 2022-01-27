f1fa057716303c342502bf06aedd3dce6373f3a1
fabio romao <fromao@mkmservice.com>
2022-01-27 20:38:42 -0300

    fix atequando1

--- eks-teste/eks/providers.tf
+++ eks-teste/eks/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "romaoeks-eks-eks"
-    key    = "idiota/master/mais/master"
-    region = "sa-east-1"
+    #bucket = "romaoeks-eks-eks"
+    #key    = "idiota/master"
+    #region = "sa-east-1"
   }
 }
