30b1370422c842e867f5faf950aeaeb5f6e97e86
fabio romao <fromao@mkmservice.com>
2022-01-28 16:29:17 -0300

    remove backend teste

--- eks-teste/eks/backend.tf
+++ eks-teste/eks/backend.tf
@@ -2,10 +2,10 @@
 #  region = "sa-east-1"
 #}
 
-terraform {
-  backend "s3" {
-    bucket = "romaoeks-eks-eks"
-    key    = "romaoeks-eks1"
-    region = "sa-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "romaoeks-eks-eks"
+#    key    = "romaoeks-eks1"
+#    region = "sa-east-1"
+#  }
+#}
