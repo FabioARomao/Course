eefa97c5562fb4a5259906f9d36bd1d16eccee03
fabio romao <fromao@mkmservice.com>
2022-01-28 10:09:05 -0300

    new

--- eks-teste/eks/backend.tf
+++ eks-teste/eks/backend.tf
@@ -1,13 +1,7 @@
-provider "aws" {
-  region = "sa-east-1"
-}
-
 terraform {
-    backend "s3" {
+  backend "s3" {
     bucket = "romaoeks-eks-eks"
-    encrypt = false
-    key = "idiota"
+    key = "romaoeks-eks"
     region = "sa-east-1"
-    #profile = "terraeks" # you have to give the profile name here. not the variable("${var.AWS_PROFILE}")
   }
 }
