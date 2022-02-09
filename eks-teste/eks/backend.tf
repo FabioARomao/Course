c0a12c76990024e270431b6919a6a32c32554c66
fabio romao <fromao@mkmservice.com>
2022-02-09 15:46:34 -0300

    update backend

--- eks-teste/eks/backend.tf
+++ eks-teste/eks/backend.tf
@@ -1,11 +1,12 @@
-#provider "aws" {
-#  region = "sa-east-1"
-#}
-
-#terraform {
-#  backend "s3" {
-#    bucket = "romaoeks-eks-eks"
-#    key    = "romaoeks-eks1"
-#    region = "sa-east-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    encrypt                 = true
+    bucket                  = "romaos3-backend-eks"
+    dynamodb_table          = "romaos3table-state-lock-dynamo"
+    region                  = "sa-east-1"
+    workspace_key_prefix    = "testing"
+    key                     = "eks/terraform.tfstate"
+    profile                 = "novachave"
+    shared_credentials_file = "~/.aws/credentials"
+  }
+}
