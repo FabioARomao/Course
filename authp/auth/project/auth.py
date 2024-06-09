fc8ff4de78be65c3607d0dbcf81702577a200c78
Fabio Romao <fabioaparecidoromao@gmail.com>
2024-06-08 23:07:16 -0300

    adjust

--- authp/auth/project/auth.py
+++ authp/auth/project/auth.py
@@ -2,7 +2,9 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash,
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
-from flask_login import current_user, login_required, logout_user, login_user
+from flask_login import LoginManager, UserMixin, current_user, login_required, logout_user, login_user
+import pandas as pd
+import matplotlib.pyplot as plt
 
 auth = Blueprint('auth', __name__)
 
@@ -10,9 +12,11 @@ auth = Blueprint('auth', __name__)
 #def login():
 #    return render_template('login.html')
 
+
 @auth.route('/login', methods=['GET','POST'])
 def login():
     if current_user.is_authenticated:
+        print(current_user)
         return redirect(url_for('main.profile'))
     if request.method == 'POST':
         email = request.form.get('email')
@@ -20,6 +24,7 @@ def login():
         #remember = True if request.form.get('remember') else False
         user = User.query.filter_by(email=email).first()
         if user and check_password_hash(user.password, password):
+            #user = User(email)
             login_user(user)
             print(email, password)
             return redirect(url_for('main.profile'))
@@ -31,10 +36,40 @@ def login():
             return redirect(url_for('auth.login'))
         
     return render_template('login.html')
-    
-#@auth.route('/signup')
-#def signup():
-#    return render_template('signup.html')
+
+users = {
+    'user1': {'id': 1, 'email': 'fabio1@fabio1.com.br', 'data_file': 'users_data/user1.csv'},
+    'user2': {'id': 2, 'email': 'fabio@fabio.com.br', 'data_file': 'users_data/user2.json'}
+}
+
+@auth.route('/dashboard')
+@login_required
+def dashboard():
+    user_data_file = None
+    for email, user in users.items():
+        if user['id'] == current_user.id:
+            user_data_file = user['data_file']
+            break
+    print(user_data_file)
+    if not user_data_file:
+        flash('Nao exitem dados para seu Usuario')
+        return render_template('dashboard.html', name=current_user.id, data_available=False)
+    #elif current_user.username == 'Guest':
+    #    flash('Please login in to see your stream!', 'error')
+    #    return redirect(url_for('index'))
+    if user_data_file.endswith('.csv'):
+        data = pd.read_csv(user_data_file)
+    elif user_data_file.endswith('json'):
+        data = pd.read_json(user_data_file)
+    else:
+        data = pd.DataFrame()
+
+    if not data.empty:
+        plot = data.plot(kind='bar')
+        plt.savefig('static/plot.png')
+        plt.close()
+    return render_template('dashboard.html', name=current_user.id, data_available=not data.empty)
+
 
 @auth.route('/signup', methods=['GET','POST'])
 def signup():
@@ -50,7 +85,7 @@ def signup():
         else:
             pass
     
-        if user:
+        if email in user:
             flash('Email ja esta registrado, faca login', 'warning')
             return redirect(url_for('auth.login'))
     
--- authp/auth/project/main.py
+++ authp/auth/project/main.py
@@ -1,10 +1,9 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 from . import db
 from .models import User
 import os
 from . import leitura
-
 
 main = Blueprint('main', __name__)
 
@@ -13,56 +12,36 @@ def index():
     return render_template('index.html')
     #return 'Index'
 
+@main.route('/usuario', methods = ['GET'])
+@login_required
+def usuario():
+    users = User.query.all()
+    usuarios = [{"nome": user.nome, "email": user.email} for user in users] #inserir no for , "data_file": user.data_file
+    #print(usuarios)
+    return render_template('usuario.html', usuarios=usuarios)
+
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.nome)
 
-@main.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html', name=current_user.nome)
-
-@main.route('/usuarios')
-#login_required
-def usuario():
-    language = request.args.get('language')
-    nomes = leitura.leitura_de_arquivo("babys.json")
-    html_nomes = ""
-    for nome in nomes:
-        html_nomes += "<h1>{}</h1>".format(nome)
-    return render_template('usuario.html', html_nomes)   
-    #return html_nomes
-    #return render_template('usuario.html', name=current_user.nome)
-
-@main.route('/usuarios1', methods = ['GET'])
+#@main.route('/callback')
 #@login_required
-def usuario1():
-    users = User.query.all()
-    print(type(users))
-    user_list = [{"nome": user.nome, "email": user.email} for user in users] #inserir no for , "data_file": user.data_file
-    print(user_list)
-    #del[user_list]['nome':'']
-    return render_template('usuario.html', users=user_list)
-
-@main.route('/callback')
-@login_required
-def callback():
-    language = request.args.get('language')
-    nomes = leitura.leitura_de_arquivo("babys.json")
-    html_nomes = ""
-    for nome in nomes:
-        html_nomes += "<h1>{}</h1>".format(nome)
-    return html_nomes
-
-@main.route('/callback2')
-@login_required
-def callback2():
-    language = request.args.get('language')
-    nomes = User.query.all()
-    html_nomes = ""
-    for nome in nomes:
-        html_nomes += "<h1>{}</h1>".format(nome)
-    print(html_nomes)    
-    return html_nomes
-
+#def callback():
+#    language = request.args.get('language')
+#    nomes = leitura.leitura_de_arquivo("babys.json")
+#    html_nomes = ""
+#    for nome in nomes:
+#        html_nomes += "<h1>{}</h1>".format(nome)
+#    return html_nomes
+#
+#@main.route('/callback2')
+#@login_required
+#def callback2():
+#    language = request.args.get('language')
+#    nomes = User.query.all()
+#    html_nomes = ""
+#    for nome in nomes:
+#        html_nomes += "<h1>{}</h1>".format(nome)
+#    print(html_nomes)    
+#    return html_nomes
